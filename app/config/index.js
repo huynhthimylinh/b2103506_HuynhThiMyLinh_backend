8bca330ce408d0260f814a8ec38820c594839b02
huynhthimylinh <huynhthimylinh55@gmail.com>
2023-10-04 00:06:31 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -4,8 +4,8 @@ const config = {
   },
 
   db: {
-    uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook",
-  }
+    uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
+    }
 };
 
 module.exports = config;
